27bc2400c8d14b8ffc348be1861762866ad1c3d8
kellypham16 <kelly.pham@thescore.com>
2023-08-03 14:04:46 -0400

    add rest of pre-prompts

--- test.py
+++ test.py
@@ -22,3 +22,53 @@ response = co.generate(
     max_tokens=1000
 )
 print(response[0].text)
+
+pre_prompt = "If a sport is referenced in the following prompt, please output the name of the sport and nothing else. Otherwise, say 'N/A'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
+
+pre_prompt = "If game statistics are referenced in the following prompt, please output the name of the game statistic and nothing else. Otherwise, say 'N/A'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
+
+pre_prompt = "If the name of an NBA player is referenced in the following prompt, please output the name of the player and nothing else. Otherwise, say 'N/A'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
+
+pre_prompt = "If winning referenced in the following prompt, please output 'Win'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
+
+pre_prompt = "If losing referenced in the following prompt, please output 'Loss'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
