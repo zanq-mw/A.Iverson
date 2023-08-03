b721fbaa06a3c820fc5f2c1b1d5468363e1fb84f
Zan Qureshi <zan.qureshi@thescore.com>
2023-08-03 13:29:15 -0400

    second commit

--- test.py
+++ test.py
@@ -2,8 +2,23 @@ import cohere
 api_key = 's1jvD0hXSVagqr4xxCo2wO9WGosYttDhRoytAX6h'
 co = cohere.Client(api_key)
 
+pre_prompt = "If money is referenced in the following prompt, please output only that amount prepended with a dollar sign and nothing else. Otherwise, say 'N/A'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
 response = co.generate(
-  prompt='Explain what is moneyline betting?',
-  max_tokens= 1000
+    pre_prompt + prompt,
+    max_tokens=1000
+)
+print(response[0].text)
+
+
+pre_prompt = "If an NBA is referenced in the following prompt, please output only the full name and nothing else. Otherwise, say 'N/A'. Here is the prompt: "
+
+prompt = input("Prompt: ")
+
+response = co.generate(
+    pre_prompt + prompt,
+    max_tokens=1000
 )
 print(response[0].text)
