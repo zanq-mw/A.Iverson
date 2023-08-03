dfa8db1897f21f1e5aaa05e5a40324faccc5b924
Anders Tai <andersyftai@gmail.com>
2023-08-03 15:32:49 -0400

    Initial app

---
+++ front-end/A.Iverson/A.Iverson/Extensions/View.swift
@@ -0,0 +1,41 @@
+//
+//  View.swift
+//  A.Iverson
+//
+//  Created by Anders Tai on 2023-08-03.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
---
+++ front-end/A.Iverson/A.IversonUITests/A_IversonUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  A_IversonUITestsLaunchTests.swift
+//  A.IversonUITests
+//
+//  Created by Anders Tai on 2023-08-03.
+//
+
+import XCTest
+
+final class A_IversonUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
