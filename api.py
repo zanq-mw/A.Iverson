78775a458f52ae7405a943e5c4ba6b6565aeb414
kellypham16 <kelly.pham@thescore.com>
2023-08-06 21:13:21 -0400

    change api anme

--- api.py
+++ api.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # run `uvicorn api:app --reload` to start server
 # go to http://127.0.0.1:8000/docs to test
 # use this every time the user sends a message
-@app.post("/receive_message")
-def receive_message(user_message: UserMessage):
+@app.post("/message")
+def message(user_message: UserMessage):
     response = main.start_workflow(user_message)
     return response
