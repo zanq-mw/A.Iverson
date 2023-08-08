e35b9d99ffba1df862fbc811a6ef44469d5f1e3e
Kaifee Haque <kaifee.haque@thescore.com>
2023-08-08 09:42:02 -0400

    Use new Q&A model

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ config["api_keys"] = {
 }
 
 config["models"] = {
-    "generate_answers": "cf0924a2-7c7f-43a3-962f-4c83403d92fe-ft",
+    "generate_answers": "ba443efe-b248-4d09-a6a4-f682a3fa4445-ft",
     "teams": "ccaf2c54-69a1-4584-9cb1-e4145d8b9e48-ft",
     "points": "6ff555ab-8547-449a-ad8b-75c94d3ad4da-ft",
     "prices": "fd79bd6e-0036-492c-88ed-60ec4395d1f3-ft"
